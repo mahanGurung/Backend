160cc69919aceaa42333db0af16100c009641bd4
mahanGurung <mahangurung5@gmail.com>
2023-03-03 22:08:12 +0545

    getCategories update

--- getCategories.php
+++ getCategories.php
@@ -3,25 +3,24 @@
 include 'DatabaseConfig.php';
 include 'helper_functions/authentication_functions.php';
 //get categories from the database
- $categories = "SELECT * FROM categories";
-    $result = mysqli_query($con, $categories);
-    if ($result) {
-        $data = [];
-        while ($row = mysqli_fetch_assoc($result)) {
-            $data[] = $row;
-        }
-        echo json_encode(
-            [
-                'success' => true,
-                'data' => $data,
-                'message' => "Catgories fetched successfully"
-            ]
-        );
-    } else {
-        echo json_encode(
-            [
-                'success' => false,
-                'message' => 'Error fetching categories'
-            ]
-        );
+ if(!$con){
+    echo json_encode(
+        [
+            'success' => false,
+            'message' => 'Error fetching categories'
+        ]
+    );
+ }
+
+ $list = array();
+ $categories = "SELECT category_id, name FROM categories";
+ $result = mysqli_query($con, $categories);
+//  $result = $con->query("SELECT category_id, name FROM categories");
+if ($result) {
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        $list[]  = $row;
     }
+
+    echo json_encode($list);
+ }
