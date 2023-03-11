95e5fe20697414b82b59c7c62ffc0392c1b1852e
mahanGurung <mahangurung5@gmail.com>
2023-03-11 23:27:31 +0545

    get and toggle doctor

--- doctorToggle.php
+++ doctorToggle.php
@@ -1,11 +1,15 @@
 <?php
-if (isset($_POST['Id'])) {//Form was submitted
-    (isset($_POST['approved'])) ? $approved = 1 : $approved = 0;
+include 'DatabaseConfig.php';
+if (isset($_POST['Id']) && isset($_POST['approved'])) {//Form was submitted
+//  ? $approved = 1 : $approved = 0;
     $machine_id = $_POST['Id'];
+    $approved = $_POST['approved'];
     //Update DB
-    $db = new PDO('mysql:host=localhost;dbname=hajur_ko_doctor;charset=utf8mb4', 'root', '');
-    $update = $db->prepare("UPDATE `doctors` SET `approved` = ? WHERE `Id` = ? LIMIT 1;");
-    $update->execute([$approved, $machine_id]);
+    
+    $update = ("UPDATE `doctors` SET `approved` = '$approved' WHERE `Id` = '$machine_id' LIMIT 1;");
+    $update = mysqli_query($con, $update);
+                  
+
 } else {//Page was loaded
     echo json_encode([$approved = 'approved']);
 }
@@ -13,7 +17,7 @@ if ($approved) {//approved = 1 (on)
     $data=['success'=>true, 'message'=>'Category added successfully.'];
     echo json_encode($data);
 } else {
-    $data=['success'=>false, 'message'=>'Something went wrong while adding category. Please try again.'];
+    $data=['success'=>false, 'message'=>'Something went wrong while adding doctor. Please try again.'];
     echo json_encode($data);
 }
 ?>
--- getDoctors.php
+++ getDoctors.php
@@ -4,8 +4,8 @@ include 'DatabaseConfig.php';
 include 'helper_functions/authentication_functions.php';
 //get doctors from the database
  $doctors = "SELECT doctors.Id, doctors.name, doctors.image, doctors.description,
-    doctors.price, doctors.Merchant_ID as hospital, categories.name as category FROM doctors 
-    join hospitals on doctors.Merchant_ID = hospitals.Merchant_ID join categories on doctors.category_id = categories.category_id";
+    doctors.price, doctors.approved,doctors.Merchant_ID as hospital, categories.name as category FROM doctors 
+    join merchant on doctors.Merchant_ID = merchant.Merchant_ID join categories on doctors.category_id = categories.category_id";
     $result = mysqli_query($con, $doctors);
     if ($result) {
         $data = [];
@@ -16,7 +16,7 @@ include 'helper_functions/authentication_functions.php';
             [
                 'success' => true,
                 'data' => $data,
-                'message' => "Products fetched successfully"
+                'message' => "doctor fetched successfully"
             ]
         );
     } else {
