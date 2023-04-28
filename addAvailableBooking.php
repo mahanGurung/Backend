61b5caa8c16af62c6c02f221ca47ce3f4f78bf6d
mahanGurung <mahangurung5@gmail.com>
2023-04-28 21:47:24 +0545

    add booking

--- addAvailableBooking.php
+++ addAvailableBooking.php
@@ -10,9 +10,9 @@ include 'helper_functions/authentication_functions.php';
     && isset($_POST['is_daily']) 
     // && isset($_POST['Date'])
     && isset($_POST['show_on_weekends'])
+    && isset($_POST['is_visible']) 
      
-     
-    && isset($_POST['merchant_id']) 
+    && isset($_POST['merchantId']) 
     && isset($_POST['quantity'])
     ) {
         $doctor_id = $_POST['doctor_id'];
@@ -20,22 +20,25 @@ include 'helper_functions/authentication_functions.php';
         $Date = date('Y-m-d');
         $show_on_weekends = $_POST['show_on_weekends'];
         $quantity = $_POST['quantity'];
+        $is_visible = $_POST['is_visible'];
     
         
-        $merchant_id = $_POST['merchant_id'];
+        $merchantId = $_POST['merchantId'];
          //getimage
         
-         $check_name = "SELECT * FROM availables_bookings WHERE doctor_id = '$doctor_id' AND Date = '$Date'";
-         $result = mysqli_query($con, $check_name);
-         $count = mysqli_num_rows($result);
-         if ($count > 0) {
-             echo json_encode(
-                 [
-                     'success' => false,
-                     'message' => 'Booking already exits'
-                 ]
-                 ); 
-            }else {
+        //  $check_name = "SELECT * FROM availables_bookings WHERE doctor_id = '$doctor_id' AND Date = '$Date' and merchantId = '$merchantId'";
+        //  $result = mysqli_query($con, $check_name);
+        //  $count = mysqli_num_rows($result);
+         $int = 1;
+         while($int <= $quantity){
+        //  if ($count > 0) {
+        //      echo json_encode(
+        //          [
+        //              'success' => false,
+        //              'message' => 'Booking already exits'
+        //          ]
+        //          ); 
+        //     }else {
 
                 //upload image
                 
@@ -51,10 +54,15 @@ include 'helper_functions/authentication_functions.php';
                           //   }
                        
                           // }
-                          addbooking($doctor_id, $is_daily, $Date, $show_on_weekends,$quantity,$merchant_id);
-                       }
-      
-                     } else {
+                          addbooking($doctor_id, $is_daily, $Date, $show_on_weekends,$is_visible,$quantity,$merchantId);
+                          $int++;
+                       
+                        }
+                    
+                    }
+                    
+                    //  }
+                      else {
         echo json_encode(
             [
                 'success' => false,
